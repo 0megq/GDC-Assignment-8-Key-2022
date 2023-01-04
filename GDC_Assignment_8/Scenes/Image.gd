2edc219bd56c6fb81e9b28e026b7824b6dd18edd
0megq <nvatanshenas55@gmail.com>
2023-01-03 18:54:17 -0800

    Fixed some inconsistencies

--- GDC_Assignment_8/Scenes/Image.gd
+++ GDC_Assignment_8/Scenes/Image.gd
@@ -4,11 +4,11 @@ extends Area2D
 var touching_mouse: bool
 
 
-
-
 func _on_Sprite_mouse_entered() -> void:
 	touching_mouse = true
 
 
 func _on_Sprite_mouse_exited() -> void:
 	touching_mouse = false
+
+
--- GDC_Assignment_8/Scripts/Main.gd
+++ GDC_Assignment_8/Scripts/Main.gd
@@ -6,22 +6,26 @@ export var image_scene: PackedScene
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("draw"):
-		create_sprite()
+		create_image()
 		
 	if Input.is_action_pressed("erase"):
-		delete_sprite()
+		delete_image()
 	
 
-# Creates the sprite
-func create_sprite() -> void:
+# Creates the image
+func create_image() -> void:
 	var image: Area2D = image_scene.instance()
 	add_child(image)
 	
 	image.position = get_viewport().get_mouse_position()
 
 	
-func delete_sprite() -> void:
-	var children: Array = get_children()
+func delete_image() -> void:
+	var children: Array = get_children() # Children
 	for child in children:
 		if child.touching_mouse: # Touching mouse is a variable on each child
 			child.queue_free()
+
+
+
+
